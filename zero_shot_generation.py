86425ebc479debc6254f6310f9b55bea8edf4098
yanan <yanan@yanandeMacBook-Air.local>
2021-04-08 10:58:42 +0800

    update

--- zero_shot_generation.py
+++ zero_shot_generation.py
@@ -7,6 +7,7 @@
 'Event', 'Learned', 'Notion', 'Wikipedia', 'Books', 'Extract', 'Confessions', 'Conspiracy', 
 'Links', 'Narcissus', 'Relationship', 'Relationships', 'Reviews', 'News', 'Translation', 'multilingual']
 '''
+import pandas as pd 
 from transformers import pipeline
 import os, argparse, random
 parser = argparse.ArgumentParser()
@@ -14,44 +15,31 @@ parser.add_argument("--model", default="", type=str)
 args = parser.parse_args()
 
 import torch
-from transformers import CTRLTokenizer, CTRLLMHeadModel
-tokenizer_ctrl = CTRLTokenizer.from_pretrained('ctrl')
+#from transformers import CTRLTokenizer, CTRLLMHeadModel
+#tokenizer_ctrl = CTRLTokenizer.from_pretrained('ctrl')
 #model = CTRLLMHeadModel.from_pretrained('ctrl')
-control_codes = tokenizer_ctrl.control_codes.keys()
+#control_codes = tokenizer_ctrl.control_codes.keys()
 
-labels = ["Society & Culture",
-        "Science & Mathematics",
-        "Health",
-        "Education & Reference",
-        "Computers & Internet",
-        "Sports",
-        "Business & Finance",
-        "Entertainment & Music",
-        "Family & Relationships",
-        "Politics & Government",
-        "palestine",
-        "president obama",
-        "microsoft",
-        "Economy",
-        #"world news",
-        "Business",
-        "science and technology"]
+dfcodes = pd.read_csv("label_codes.tsv", sep='\t')
 
-
-
-model  = pipeline("text-generation", model=args.model, device=0) #  
+model  = pipeline("text-generation", model=args.model, device=0)
 
 while 1:
     label = random.sample(labels, 1)[0]
-    results = model(label, max_length=250, do_sample=True, top_p=0.9, top_k=0, num_return_sequences=5)
+    if args.model == 'gpt2':
+        results = model(label, max_length=250, do_sample=True, top_p=0.9, top_k=0, num_return_sequences=10)
+    elif args.model == 'ctrl':
+        results = model(label, max_length=250, do_sample=True, top_p=0.9, top_k=0, \
+            repetition_penalty=1.2, num_return_sequences=10)
+    else:
+        raise KeyError("args.model illegal!")
+
     for row in results:
         content = row['generated_text']
-        content = content.replace(label, '').replace('\t',' ')
+        content = content.replace(label, '').replace('\t',' ').replace('\n',' ')
         if len(content.split(' ')) <= 30:
             continue 
-        print(label, '\t', content.replace('\n',' '))
-
-
+        print(label, '\t', content)
 
 
 
