b24330b08b0112e713cd3a5beafa98e257dee715
yanan <yanan@yanandeMacBook-Air.local>
2021-04-23 19:02:05 +0800

    update

--- content_generation.py
+++ content_generation.py
@@ -39,25 +39,30 @@ while ix < ds.df_train.shape[0]:
 
 
 
+for dsn in ['ag','yahoo']:
+    ds = load_data(dataset=dsn, samplecnt=-1)
+    df_t = pd.concat([ds.df_test, ds.df_train])
+    df_t['label'] = 1
+    del df_t['title']
 
+    df_f = pd.read_csv("df_nli_filter_ctrl_ag.csv")
+    df_f['label'] = 0
 
-df_t = pd.concat([ds.df_test, ds.df_train])
-df_t['label'] = 1
-del df_t['title']
+    if df_f.shape[0] > df_t.shape[0]:
+        df_f = df_f.sample(df_t.shape[0])
 
-df_f = df.sample(df_t.shape[0])
-df_f['label'] = 0
+    if df_f.shape[0] < df_t.shape[0]:
+        df_t = df_t.sample(df_f.shape[0])
 
+    df = pd.concat([df_f, df_t])
+    from sklearn.model_selection import train_test_split
+    df_train, df_test = train_test_split(df, test_size=0.2)
+    (x_train, y_train),  (x_test, y_test), num_classes = get_keras_data(df_train, df_test)
 
-df = pd.concat([df_f, df_t])
-from sklearn.model_selection import train_test_split
-df_train, df_test = train_test_split(df, test_size=0.2)
-(x_train, y_train),  (x_test, y_test), num_classes = get_keras_data(df_train, df_test)
-
-model = get_model_albert(num_classes)
-model.fit(
-            x_train, y_train, batch_size=64, epochs=12, validation_data=(x_test, y_test), verbose=1,
-            callbacks = [EarlyStopping(monitor='val_acc', patience=3, mode='max')]
-        )
+    model = get_model_albert(num_classes)
+    model.fit(
+                x_train, y_train, batch_size=64, epochs=12, validation_data=(x_test, y_test), verbose=1,
+                callbacks = [EarlyStopping(monitor='val_acc', patience=3, mode='max')]
+            )
 
 
