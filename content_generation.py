6510ad3b49f656dd6c17ba48ad4c7785c98f85ea
yanan <yanan@yanandeMacBook-Air.local>
2021-04-23 17:19:40 +0800

    update

--- content_generation.py
+++ content_generation.py
@@ -5,7 +5,7 @@ from load_data import *
 from transblock import * 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--dataset", default="", type=str)
+parser.add_argument("--dataset", default="ag", type=str)
 args = parser.parse_args()
 
 ds = load_data(dataset=args.dataset, samplecnt=-1)
@@ -13,16 +13,44 @@ generator  = pipeline("text-generation", model='gpt2', device=0)
 
 
 generator_col = []
-for ix, row  in ds.df_train.iterrows():
-    results = generator(row['content'], max_length=200, do_sample=True, top_p=0.9, top_k=0, \
-                repetition_penalty=1, num_return_sequences=16) 
-    generated_texts_filter = [ sent['generated_text'].replace(row['content'], '').replace('\t',' ').replace('\n',' ')\
-            for sent in  results ]
-    generator_col.append('\t'.join(generated_texts_filter))
+ix = 0
+while ix < ds.df_train.shape[0]:
+    dfi = ds.df_train[ix:(ix+64)]
+    results = generator(dfi['content'].tolist(), max_length=200, do_sample=True, top_p=0.9, top_k=0, \
+                repetition_penalty=1, num_return_sequences=8) 
+
+    generated_texts_filters = []
+    for ori, gens in zip(dfi['content'].tolist(), results):
+        generated_texts_filter = [ sent['generated_text'].replace(ori, '').replace('\t',' ').replace('\n',' ')\
+                for sent in  gens ]
+        generated_texts_filters.append('\t'.join(generated_texts_filter))
+
+    generator_col.extend(list(zip(dfi['label'].tolist(), generated_texts_filters)))
+
     if len(generator_col) % 2000 == 0:
         print(args.dataset, ' ==> ', len(generator_col), ' /', ds.df_train.shape[0])
- 
+    ix += 64
+
 ds.df_train['content_g'] = generator_col
 
 ds.df_train.to_csv("{}_df_train.csv".format(args.dataset), index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
