279a4e88351077c453a9d83c9770a9dd68916e1d
yanan <yanan@yanandeMacBook-Air.local>
2021-04-23 15:55:23 +0800

    update

--- content_generation.py
+++ content_generation.py
@@ -12,7 +12,7 @@ generator  = pipeline("text-generation", model='gpt2', device=0)
 generator_col = []
 for ix, row  in ds.df_train.iterrows():
     results = generator(row['content'], max_length=250, do_sample=True, top_p=0.9, top_k=0, \
-                repetition_penalty=1, num_return_sequences=16) 
+                repetition_penalty=1, num_return_sequences=32) 
     generated_texts_filter = [ sent['generated_text'].replace(row['content'], '').replace('\t',' ').replace('\n',' ')\
             for sent in  results ]
     generator_col.append('\t'.join(generated_texts_filter))
