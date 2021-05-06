d3923fa2d7012d0c5d35d85371a71b57072e8a18
yanan <yanan@yanandeMacBook-Air.local>
2021-05-06 09:09:22 +0800

    update

--- zsl_pure_classifier.py
+++ zsl_pure_classifier.py
@@ -67,8 +67,8 @@ print(args.model, ' loaded')
 ds = load_data(dataset=args.dsn)
 labels_candidate = list(ds.df['label'].unique())
 print(args.dsn, ' ==>', labels_candidate)
-if args.dsn == 'ag':
-    ds.df_test = ds.df_test.loc[ds.df_test['label']!='World']
+# if args.dsn == 'ag':
+#     ds.df_test = ds.df_test.loc[ds.df_test['label']!='World']
 if args.dsn in ['ag','yahoo','pop','dbpedia']:
     acc = get_acc(ds.df_test, labels_candidate)
 else:
