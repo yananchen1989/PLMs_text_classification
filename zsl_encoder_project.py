a59f76fc936f99e8520d88664d37a89758c26065
yanan <yanan@yanandeMacBook-Air.local>
2021-05-15 13:39:46 +0800

    update

--- zsl_encoder_project.py
+++ zsl_encoder_project.py
@@ -39,16 +39,16 @@ class encoder():
         return embeds
 
 
-def insert_label(sent, label, rep=0.1):
-    tokens = sent.split(' ')
-    for i in range(int(len(tokens)*rep )):
-        ix = random.randint(0, len(tokens) - 1)
-        #tokens.insert(ix, label)
-        tokens[ix] = label
-    return ' '.join(tokens)
+# def insert_label(sent, label, rep=0.1):
+#     tokens = sent.split(' ')
+#     for i in range(int(len(tokens)*rep )):
+#         ix = random.randint(0, len(tokens) - 1)
+#         #tokens.insert(ix, label)
+#         tokens[ix] = label
+#     return ' '.join(tokens)
 
-m = 'dan'
-enc = encoder(m)
+# m = 'dan'
+# enc = encoder(m)
 
 # dsn = '20news'
 # ds = load_data(dataset=dsn)
@@ -70,57 +70,57 @@ enc = encoder(m)
 # acc = df_simis.loc[df_simis['pred']==df_simis['label']].shape[0] / df_simis.shape[0]
 # print('enc:', m,  'dsn:', dsn, '  acc==>', acc)
 
-base = []
-with open('../datasets_aug/cnn_dailymail_stories.csv', 'r') as f:
-    for line in f:
-        if not line.strip() or len(line.strip().split(' ')) <= 20:
-            continue
-        base.append(line.strip().replace('"',''))
+# base = []
+# with open('../datasets_aug/cnn_dailymail_stories.csv', 'r') as f:
+#     for line in f:
+#         if not line.strip() or len(line.strip().split(' ')) <= 20:
+#             continue
+#         base.append(line.strip().replace('"',''))
 
-df_base = pd.DataFrame(random.sample(base, 1000000), columns=['content'])
-del base 
-gc.collect()
-print('df base==>', df_base.shape[0])
-#for dsn in ['yahoo','dbpedia','nyt','pop','20news','uci']:
+# df_base = pd.DataFrame(random.sample(base, 1000000), columns=['content'])
+# del base 
+# gc.collect()
+# print('df base==>', df_base.shape[0])
+# #for dsn in ['yahoo','dbpedia','nyt','pop','20news','uci']:
 
-dsn = 'uci'  
-ds = load_data(dataset= dsn)
+# dsn = 'uci'  
+# ds = load_data(dataset= dsn)
 
-embeds_df_cnndm = enc.infer(df_base['content'].tolist(), batch_size = 512) 
+# embeds_df_cnndm = enc.infer(df_base['content'].tolist(), batch_size = 512) 
 
-for l in ds.df['label'].unique():
-    embed_label = enc.infer([l], batch_size=16)
-    simis = cosine_similarity(embed_label, embeds_df_cnndm)
-    df_base[l] = simis[0]
-    print(l)
+# for l in ds.df['label'].unique():
+#     embed_label = enc.infer([l], batch_size=16)
+#     simis = cosine_similarity(embed_label, embeds_df_cnndm)
+#     df_base[l] = simis[0]
+#     print(l)
     
-ds.df_test = ds.df_test.sample(1000)
-labels = ds.df_test['label'].tolist()
-contents = ds.df_test['content'].tolist()
-contents_test_embed = enc.infer(contents, batch_size=512)
+# ds.df_test = ds.df_test.sample(1000)
+# labels = ds.df_test['label'].tolist()
+# contents = ds.df_test['content'].tolist()
+# contents_test_embed = enc.infer(contents, batch_size=512)
 
 
-simiall = cosine_similarity(contents_test_embed, embeds_df_cnndm)
+# simiall = cosine_similarity(contents_test_embed, embeds_df_cnndm)
 
-acc = []
-for ii in range(ds.df_test.shape[0]):
-    label = labels[ii]
-    content = contents[ii]
-    test_embed = contents_test_embed[ii]
-    scores = simiall[ii]#cosine_similarity(test_embed.reshape(1,-1), embeds_df_cnndm)
-    #assert scores.shape[0] == samplecnt
-    label_score = {}
-    for l in ds.df['label'].unique():
-        score_l = np.dot(scores, df_base[l].values)
-        label_score[l] = score_l
+# acc = []
+# for ii in range(ds.df_test.shape[0]):
+#     label = labels[ii]
+#     content = contents[ii]
+#     test_embed = contents_test_embed[ii]
+#     scores = simiall[ii]#cosine_similarity(test_embed.reshape(1,-1), embeds_df_cnndm)
+#     #assert scores.shape[0] == samplecnt
+#     label_score = {}
+#     for l in ds.df['label'].unique():
+#         score_l = np.dot(scores, df_base[l].values)
+#         label_score[l] = score_l
 
-    label_score_sort = sorted(label_score.items(), key=lambda item: item[1], reverse=True)
-    pred = label_score_sort[0][0]
-    if pred == label:
-        acc.append(1)
-    else:
-        acc.append(0)
-print('acc==>', sum(acc) / len(acc))
+#     label_score_sort = sorted(label_score.items(), key=lambda item: item[1], reverse=True)
+#     pred = label_score_sort[0][0]
+#     if pred == label:
+#         acc.append(1)
+#     else:
+#         acc.append(0)
+# print('acc==>', sum(acc) / len(acc))
 
 
 
