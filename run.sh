588c6a32237f70db54ba21fedb21b3bca8ecc7a7
yanan <yanan@yanandeMacBook-Air.local>
2021-05-17 17:27:00 +0800

    update

--- run.sh
+++ run.sh
@@ -1,22 +1,22 @@
-dsn=ag
+dsn=${1}
 samplecnt=32
 batch_size=8
 
 # baseline 
-python -u baseline_classifier.py --dsn ${dsn} --aug no --samplecnt ${samplecnt} --batch_size ${batch_size} 
+python -u baseline_classifier.py --dsn ${dsn} --aug no --samplecnt ${samplecnt} 
 
 
 # back-translation
 for lang in de fr zh 
 do
-python -u baseline_classifier.py --ds ${dsn} --aug translate --samplecnt ${samplecnt}  --batch_size ${batch_size} --lang ${lang} 
+python -u baseline_classifier.py --ds ${dsn} --aug translate --samplecnt ${samplecnt} --lang ${lang} 
 done
 
 
 # fillin masks
 for ner in 0 1
 do
-python -u baseline_classifier.py --dsn ${dsn} --aug fillin --ner_set ${ner} --samplecnt ${samplecnt}  --batch_size ${batch_size} 
+python -u baseline_classifier.py --dsn ${dsn} --aug fillin --ner_set ${ner} --samplecnt ${samplecnt} 
 done
 
 
@@ -24,6 +24,6 @@ done
 for beam in 1 2 4 8 16 32 64
 do
 #python -u baseline_classifier.py --ds ${dsn} --aug generate --generate_m ctrl --samplecnt ${samplecnt}  --batch_size ${batch_size}  > aug_${dsn}_generate_ctrl.log 
-python -u baseline_classifier.py --dsn ${dsn} --aug generate --generate_m gpt2 --beams ${beam} --samplecnt ${samplecnt}  --batch_size ${batch_size}   
+python -u baseline_classifier.py --dsn ${dsn} --aug generate --generate_m gpt2 --beams ${beam} --samplecnt ${samplecnt} 
 done
 
