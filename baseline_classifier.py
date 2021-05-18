b15057305573a05070a0a18259efe72edd40a624
yanan <yanan@yanandeMacBook-Air.local>
2021-05-18 12:15:44 +0800

    update

--- baseline_classifier.py
+++ baseline_classifier.py
@@ -30,7 +30,7 @@ parser.add_argument("--enc_m", default='dan', type=str)
 parser.add_argument("--nli_m", default="joeddav/bart-large-mnli-yahoo-answers", type=str)
 parser.add_argument("--thres", default=0.65, type=float)
 parser.add_argument("--times", default=2, type=int)
-parser.add_argument("--cap3rd", default=0.8, type=int)
+parser.add_argument("--cap3rd", default=0.8, type=float)
 
 
 args = parser.parse_args()
