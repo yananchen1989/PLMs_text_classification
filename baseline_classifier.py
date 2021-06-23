7418c88539b487381d386e06a81084ea08f8683e
yanan <yanan@yanandeMacBook-Air.local>
2021-06-23 16:44:53 +0800

    up

--- baseline_classifier.py
+++ baseline_classifier.py
@@ -28,19 +28,19 @@ parser.add_argument("--generate_m", default="gpt2", type=str)
 parser.add_argument("--batch_size", default=32, type=int)
 #parser.add_argument("--gpu", default="0", type=str)
 parser.add_argument("--model", default="albert", type=str)
-parser.add_argument("--verbose", default=1, type=int)
-parser.add_argument("--mm", default="mean", type=str)
+#parser.add_argument("--verbose", default=1, type=int)
+parser.add_argument("--checkmode", default="rank", type=str) # rank or thres
 parser.add_argument("--beams", default=100, type=int)
 parser.add_argument("--rp", default=1.0, type=float)
 parser.add_argument("--check", default='enc', type=str)
 parser.add_argument("--enc_m", default='dan', type=str)
 #parser.add_argument("--nli_m", default="joeddav/bart-large-mnli-yahoo-answers", type=str)
-parser.add_argument("--thres", default=0.65, type=float)
+#parser.add_argument("--thres", default=0.65, type=float)
 #parser.add_argument("--times", default=2, type=int)
 parser.add_argument("--cap3rd", default=0.99, type=float)
 parser.add_argument("--trunk_size", default=50, type=int)
-parser.add_argument("--dpp", default=0, type=int)
-parser.add_argument("--dpp_retain", default=0.7, type=float)
+#parser.add_argument("--dpp", default=0, type=int)
+#parser.add_argument("--dpp_retain", default=0.7, type=float)
 parser.add_argument("--max_aug_times", default=10, type=int)
 
 parser.add_argument("--eda_times", required=False, type=int, default=1, help="number of augmented sentences per original sentence")
@@ -95,7 +95,7 @@ if args.aug == 'generate':
         args.rp = 1.2
     nlp  = pipeline("text-generation", model=args.generate_m, device=device, return_full_text=False)
 
-if args.aug == 'translate':
+if args.aug == 'bt':
     tokenizer_backward = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-{}-en".format(args.lang), cache_dir="./cache")
     model_backward = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-{}-en".format(args.lang), cache_dir="./cache")
     tokenizer_forward = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-{}".format(args.lang), cache_dir="./cache")
@@ -119,7 +119,7 @@ def do_train_test(ds):
 
     history = model.fit(
         x_train, y_train, batch_size=args.batch_size, epochs=50, \
-        validation_data=(x_test, y_test), verbose=args.verbose,
+        validation_data=(x_test, y_test), verbose=1,
         callbacks = [EarlyStopping(monitor='val_acc', patience=3, mode='max')]
     )
 
@@ -152,60 +152,67 @@ def synthesize(ds, max_len, seed):
         print('filtering...')
         for ii in range(ds.df_train.shape[0]):
             if args.check != 'no':
-                if args.check in ['enc','double']:
+                if args.check in ['enc']:
                     ori_sentence = contents[ii]
                     ori_embed = enc.infer([ori_sentence])
                     syn_sentences = [sentence['generated_text'] for sentence in results[ii]]
                     syn_embeds = enc.infer(syn_sentences)
                     simis = cosine_similarity(ori_embed, syn_embeds)
                     df_simi = pd.DataFrame(zip(syn_sentences, simis[0]), columns=['content','simi'])
-                    df_simi.sort_values(by=['simi'], ascending=False, inplace=True)
-                    df_simi_filer = df_simi.loc[df_simi['simi']>= args.thres]
-                    print(args.dsn, 'enc ==> left', df_simi_filer.shape[0], 'of', len(results[ii]))  
-                    if df_simi_filer.shape[0] == 0:
-                        continue 
-                    df_simi_filer_enc = df_simi_filer
 
-                if args.check in ['nli','double']:
+                    #df_simi_filer_enc = df_simi_filer
+
+                if args.check in ['nli']:
                     infos_trunk = []
                     for sentence in results[ii]:
                         if not sentence['generated_text']:
                             continue
                         result_nli = nlp_nli(sentence['generated_text'], labels_candidates, multi_label=False, hypothesis_template="This text is about {}.")
-                        if result_nli['scores'][0] >= args.thres and result_nli['labels'][0] == labels[ii]:                    
-                            infos_trunk.append((sentence['generated_text'], result_nli['scores'][0] ))
-                    df_simi_filer = pd.DataFrame(infos_trunk, columns=['content','simi'])
-                    print(args.dsn, 'nli ==> left', df_simi_filer.shape[0], 'of', len(results[ii]))  
-                    if df_simi_filer.shape[0] == 0:
-                        continue                    
-                    df_simi_filer_nli = df_simi_filer
+                        #if result_nli['scores'][0] >= args.thres and result_nli['labels'][0] == labels[ii]:                    
+                        infos_trunk.append((sentence['generated_text'], result_nli['scores'][0], \
+                                             result_nli['labels'][0], labels[ii] ))
+                    df_simi = pd.DataFrame(infos_trunk, columns=['content','simi','nli_label', 'ori_label'])
+                    # print(args.dsn, 'nli ==> left', df_simi_filer.shape[0], 'of', len(results[ii]))  
+                    # if df_simi_filer.shape[0] == 0:
+                    #     continue                    
+                    #df_simi_filer_nli = df_simi_filer
+                df_simi.sort_values(by=['simi'], ascending=False, inplace=True)
+                if args.checkmode == 'thres':
+                    df_simi_filer = df_simi.loc[df_simi['simi']>= args.thres]
+                elif args.checkmode =='rank':
+                    df_simi_filer = df_simi.head(1)
 
-                if args.check == 'double':
-                    # use enc and nli to filter
-                    df_simi_filer = pd.merge(df_simi_filer_enc, df_simi_filer_nli, on='content', how='inner')
-                    if df_simi_filer.shape[0] == 0:
-                        continue 
-                    beta = 0.5
-                    df_simi_filer['simi'] = df_simi_filer['simi_x']*beta + df_simi_filer['simi_y']*(1-beta)
-                    print('dsn:{} double check==>'.format(args.dsn), \
-                            'enc:', df_simi_filer_enc.shape[0], 'nli:', df_simi_filer_nli.shape[0],\
-                           'join:', df_simi_filer.shape[0])
+                assert df_simi_filer.shape[0] == 1
+                # print(args.dsn, 'enc ==> left', df_simi_filer.shape[0], 'of', len(results[ii]))  
+                # if df_simi_filer.shape[0] == 0:
+                #     continue 
+                # if args.check == 'double':
+                #     # use enc and nli to filter
+                #     df_simi_filer = pd.merge(df_simi_filer_enc, df_simi_filer_nli, on='content', how='inner')
+                #     if df_simi_filer.shape[0] == 0:
+                #         continue 
+                #     beta = 0.5
+                #     df_simi_filer['simi'] = df_simi_filer['simi_x']*beta + df_simi_filer['simi_y']*(1-beta)
+                #     print('dsn:{} double check==>'.format(args.dsn), \
+                #             'enc:', df_simi_filer_enc.shape[0], 'nli:', df_simi_filer_nli.shape[0],\
+                #            'join:', df_simi_filer.shape[0])
 
-                if args.dpp:
-                    try:
-                        dpp_sents = dpp_rerank(df_simi_filer, enc, args.dpp_retain)
-                    except:
-                        print('dpp_rerank error==>', df_simi_filer)
-                        continue 
-                else:
-                    dpp_sents = df_simi_filer['content'].tolist()
+                # if args.dpp:
+                #     try:
+                #         dpp_sents = dpp_rerank(df_simi_filer, enc, args.dpp_retain)
+                #     except:
+                #         print('dpp_rerank error==>', df_simi_filer)
+                #         continue 
+                # else:
+                #     dpp_sents = df_simi_filer['content'].tolist()
 
-                for sentence in dpp_sents:
+                for sentence in df_simi_filer['content'].tolist():
                     infos.append((sentence, labels[ii] ))
 
             elif args.check == 'no':
-                for sentence in results[ii]:
-                    infos.append((sentence['generated_text'], labels[ii] ))
+                #for sentence in results[ii]:
+                sentence = random.sample(results[ii], 1)[0]
+                infos.append((sentence['generated_text'], labels[ii] ))
 
             else:
                 raise KeyError("args.check illegal!")  
@@ -228,7 +235,7 @@ def synthesize(ds, max_len, seed):
         sentences = ds.df_train['content'].map(lambda x: augmentor.augment(x)).tolist()
         infos = zip(sentences, ds.df_train['label'].tolist())
 
-    elif args.aug == 'translate':
+    elif args.aug == 'bt':
         contents = ds.df_train['content'].tolist()
         labels = ds.df_train['label'].tolist()
         infos = []
@@ -293,7 +300,7 @@ for ite in range(args.ite):
         accs_iters.append(cur_acc)
         gain = round( (max(accs_iters) - best_val_acc_noaug) / best_val_acc_noaug, 4)
         record_log('log_corpus', \
-            ['summary==>'] + ['{}:{}'.format(k, v) for k, v in vars(args).items()] + \
+            ['summary==>'] + ['{}:{}'.format(k, v) for k, v in vars(args).items() if not k.startswith('eda_')] + \
             ['iter:{}'.format(ite), \
             'baseline_acc {}'.format(best_val_acc_noaug),
             'aug_ratio {}'.format(aug_ratio), \
