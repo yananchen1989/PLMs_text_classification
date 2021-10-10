cc6de1e962498ea64429c3e481dbe2c9e39bf0b6
yanan <yanan@yanandeMacBook-Air.local>
2021-10-10 15:08:27 +0800

    fix ppo

--- utils/ppo_config.py
+++ utils/ppo_config.py
@@ -9,41 +9,42 @@ from transformers import GPT2Tokenizer
 gpt2_tokenizer = GPT2Tokenizer.from_pretrained('gpt2', cache_dir='./cache', local_files_only=True)
 gpt2_tokenizer.pad_token = gpt2_tokenizer.eos_token 
 
-def get_ppo_trainer(ft_path, device, params):
+#def get_ppo_trainer(ft_path, device, params):
     # if ft_pattern in ['tc', 'pp']:
     #     gpt2_tokenizer.sep_token = '<|sep|>'
     #     gpt2_model_ref_trl = GPT2HeadWithValueModel.from_pretrained('gpt2_{}_ft_on_ccnews'.format(ft_pattern))
     #     gpt2_model_trl = GPT2HeadWithValueModel.from_pretrained('gpt2_{}_ft_on_ccnews'.format(ft_pattern))
         
     #elif ft_pattern in ['no']:
-    gpt2_tokenizer.sep_token = ''
-    gpt2_model_ref_trl = GPT2HeadWithValueModel.from_pretrained(ft_path)
-    gpt2_model_trl = GPT2HeadWithValueModel.from_pretrained(ft_path)
-    config = {
-        # "lm_name": "lvwerra/gpt2-imdb",
-        # "ref_lm_name": "lvwerra/gpt2-imdb",
-         "cls_model_name": "lvwerra/bert-imdb",
-        #"tk_name": "gpt2",
-        #"steps": 25600,
-        "forward_batch_size": params['forward_batch_size'],
-        "ppo_epochs": 4,   
-        #"txt_in_len": 5,
-        #"txt_out_len": 15,
-        "batch_size": params['ppo_batchsize'] ,
-        "lr": 1.41e-5,
-        "init_kl_coef":params['init_kl_coef'],
-        "target": 6,
-        "horizon":10000,
-        "gamma":1,
-        "lam":0.95,
-        "cliprange": params['cliprange'],
-        "cliprange_value": params['cliprange_value'],
-        "vf_coef":.1, 
-    }
-    gpt2_model_ref_trl.to(device)
-    gpt2_model_trl.to(device)
-    ppo_trainer = PPOTrainer(gpt2_model_trl, gpt2_model_ref_trl, **config)
-    return ppo_trainer, gpt2_model_trl, gpt2_model_ref_trl
+params = vars(args)
+gpt2_tokenizer.sep_token = ''
+gpt2_model_ref_trl = GPT2HeadWithValueModel.from_pretrained(ft_path)
+gpt2_model_trl = GPT2HeadWithValueModel.from_pretrained(ft_path)
+config = {
+    # "lm_name": "lvwerra/gpt2-imdb",
+    # "ref_lm_name": "lvwerra/gpt2-imdb",
+     "cls_model_name": "lvwerra/bert-imdb",
+    #"tk_name": "gpt2",
+    #"steps": 25600,
+    "forward_batch_size": params['forward_batch_size'],
+    "ppo_epochs": 4,   
+    #"txt_in_len": 5,
+    #"txt_out_len": 15,
+    "batch_size": params['ppo_batchsize'] ,
+    "lr": 1.41e-5,
+    "init_kl_coef":params['init_kl_coef'],
+    "target": 6,
+    "horizon":10000,
+    "gamma":1,
+    "lam":0.95,
+    "cliprange": params['cliprange'],
+    "cliprange_value": params['cliprange_value'],
+    "vf_coef":.1, 
+}
+gpt2_model_ref_trl.to(device)
+gpt2_model_trl.to(device)
+ppo_trainer = PPOTrainer(gpt2_model_trl, gpt2_model_ref_trl, **config)
+#    return ppo_trainer, gpt2_model_trl, gpt2_model_ref_trl
 
 
 def reponse(df_batch, gpt2_model_trl, maxlen, gpt2_tokenizer, device, params):
@@ -77,6 +78,9 @@ def reponse_single(df_batch, gpt2_model_trl, maxlen, gpt2_tokenizer, device, par
         response_tensor  = respond_to_batch(gpt2_model_trl, query_tensor)
         response = gpt2_tokenizer.decode(response_tensor[0], clean_up_tokenization_spaces=True, skip_special_tokens=True).strip()
 
+
+        # get reward from another component
+        train_stats = ppo_trainer.step(query_tensor, response_tensor, reward)
         # print(query_tensor.shape[1], response_tensor.shape[1] )
         # print("query==>", query)
         # print("response==>", response, '\n')
