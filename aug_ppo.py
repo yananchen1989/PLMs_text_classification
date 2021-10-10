4e5aa695e09ca9940097814cd2673a80ac994ae6
yanan <yanan@yanandeMacBook-Air.local>
2021-10-10 17:14:08 +0800

    fix augf

--- aug_ppo.py
+++ aug_ppo.py
@@ -220,7 +220,7 @@ for epoch in range(args.ppo_epoch):
         query = "[{}]".format(row['label']) + row['content']
         query_tensor = gpt2_tokenizer.encode(query, return_tensors="pt").to(device_0)
         response_tensor  = respond_to_batch(gpt2_model_trl, query_tensor, txt_len=args.maxlen)
-        response = gpt2_tokenizer.decode(response_tensor[0], clean_up_tokenization_spaces=True, skip_special_tokens=True).strip()
+        response = gpt2_tokenizer.decode(response_tensor[0], clean_up_tokenization_spaces=True, skip_special_tokens=True).strip().replace('\n',' ')
 
         # get reward from another component
         preds_test = model.predict([response], steps=1) # (128, 4)
