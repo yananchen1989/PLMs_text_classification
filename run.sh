69c839fc95eac693a2f98568fef88d8cc1600efe
yanan <yanan@yanandeMacBook-Air.local>
2021-11-17 18:06:31 -0500

    fix

--- run.sh
+++ run.sh
@@ -26,15 +26,18 @@ do
 			do
 				for dpfuture_switch in 1 0
 				do
-					for filter in no #dvrl #nli enc nsp
+					for dpfuture_cls_switch in 1 0 
 					do
-					python -u augf.py --dsn ${dsn} --samplecnt 32 --max_aug_times 1 --aug generate \
-					      --genft ${genft}  --genm ${genm} --filter ${filter} --seed ${seed} \
-					      --valid_files_cnt 16  --threads 16 --testvalid test \
-					      --dpfuture_switch ${dpfuture_switch} --dpfuture_cls_switch 1 \
-					      --candidates 128 --test_beams 64 --cls_score_thres 0.8 \
-					      --num_return_sequences 3 --gpu ${gpu} \
-	> ./log_arxiv_dpfuture_dvrl/${dsn}.generate.32.max_aug_times.1.genm.${genm}.genft.${genft}.dpfuture_switch.${dpfuture_switch}.filter.${filter}.${seed}.log 2>&1
+						for filter in no #dvrl #nli enc nsp
+						do
+						python -u augf.py --dsn ${dsn} --samplecnt 32 --max_aug_times 1 --aug generate \
+						      --genft ${genft}  --genm ${genm} --filter ${filter} --seed ${seed} \
+						      --valid_files_cnt 16  --threads 16 --testvalid test \
+						      --dpfuture_switch ${dpfuture_switch} --dpfuture_cls_switch ${dpfuture_cls_switch} \
+						      --candidates 128 --test_beams 64 --cls_score_thres 0.8 \
+						      --num_return_sequences 3 --gpu ${gpu} \
+		> ./log_arxiv_dpfuture_dvrl/${dsn}.generate.32.max_aug_times.1.genm.${genm}.genft.${genft}.dpfuture_switch.${dpfuture_switch}.dpfuture_cls_switch.${dpfuture_cls_switch}.filter.${filter}.${seed}.log 2>&1
+						done
 					done
 				done
 			done
