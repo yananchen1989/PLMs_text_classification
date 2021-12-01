a6fe5494fdd428fd68e965067c04ae7ef59a1a02
yanan <yanan@yanandeMacBook-Air.local>
2021-12-01 15:07:07 -0500

    fix

--- run.sh
+++ run.sh
@@ -1,34 +1,33 @@
 max_aug_times=1
-#samplecnt=${1}
-candidates=256
 gpu=${1}
 
-
-for i in {1..12}
+for candidates in 512 1024 256
 do
-	for samplecnt in 64 32
+	for i in {1..10}
 	do
-		seed=$RANDOM
-		#seed=$(date +"%T")
-		for dsn in ag uci nyt #yelp2 amazon2
-		do	
-			# lambda
-			python -u augf.py --dsn ${dsn} --samplecnt ${samplecnt} --max_aug_times ${max_aug_times} --aug generate \
-					      --genft lambda  --genm gpt --filter clsembed --seed ${seed} \
-					      --testvalid test --candidates ${candidates} --gpu ${gpu} \
-			> ./log_baselines/${dsn}.generate.${samplecnt}.${max_aug_times}.${candidates}.gpt.lambda.clsembed.${seed}.log 2>&1	
-			
-			###### no finetune
-			for genm in gpt t5
-			do
+		for samplecnt in 64 32
+		do
+			seed=$RANDOM
+			#seed=$(date +"%T")
+			for dsn in ag uci nyt #yelp2 amazon2
+			do	
+				# lambda
 				python -u augf.py --dsn ${dsn} --samplecnt ${samplecnt} --max_aug_times ${max_aug_times} --aug generate \
-						      --genft no  --genm ${genm} --filter nlinsp --seed ${seed} \
+						      --genft lambda  --genm gpt --filter clsembed --seed ${seed} \
 						      --testvalid test --candidates ${candidates} --gpu ${gpu} \
-			> ./log_arxiv_nlinsp/${dsn}.generate.${samplecnt}.${max_aug_times}.${candidates}.${genm}.no.${seed}.log 2>&1
+				> ./log_baselines/${dsn}.generate.${samplecnt}.${max_aug_times}.${candidates}.gpt.lambda.clsembed.${seed}.log 2>&1	
+				
+				###### no finetune
+				for genm in gpt t5
+				do
+					python -u augf.py --dsn ${dsn} --samplecnt ${samplecnt} --max_aug_times ${max_aug_times} --aug generate \
+							      --genft no  --genm ${genm} --filter nlinsp --seed ${seed} \
+							      --testvalid test --candidates ${candidates} --gpu ${gpu} \
+				> ./log_arxiv_nlinsp/${dsn}.generate.${samplecnt}.${max_aug_times}.${candidates}.${genm}.no.${seed}.log 2>&1
+				done
+		        ## end 
 			done
-	        ## end 
 		done
-	done
-done 
-
+	done 
+done
 
