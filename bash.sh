28a76f45c66466d03ec8dd23f2e89e28b6133e39
yanan <yanan@yanandeMacBook-Air.local>
2021-12-02 18:28:26 -0500

    fix

--- bash.sh
+++ bash.sh
@@ -66,17 +66,7 @@ done
 
 
 
-while true
-do
-   seed=$RANDOM
-   for samplecnt in 32 64 128
-   do
-      for dsn in ag uci  #yelp2 amazon2
-      do 
-        python -u ablation_ft.py --samplecnt ${samplecnt} --dsn ${dsn} > ablation_ft.${samplecnt}.${dsn}.${seed}.log 2>&1
-      done
-   done
-done
+
 
 
 
