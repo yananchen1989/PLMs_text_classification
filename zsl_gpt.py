356f7aa66387f452008e3f154a5fa1430071cdb1
yanan <yanan@yanandeMacBook-Air.local>
2021-12-15 11:52:04 -0500

    fix zsl classification

--- zsl_gpt.py
+++ zsl_gpt.py
@@ -5,6 +5,8 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--fbs_gen", default=32, type=int)
+parser.add_argument("--maxlen", default=64, type=int)
+parser.add_argument("--genm", default='gpt2', type=str)
 parser.add_argument("--gpu", default="0,1", type=str)
 args = parser.parse_args()
 
@@ -36,18 +38,28 @@ nli_nlp = pipeline("zero-shot-classification", model=model_nli, tokenizer=tokeni
 
 
 from transformers import pipeline
-from transformers import GPT2Tokenizer, GPT2LMHeadModel #TFGPT2LMHeadModel, TFGPT2Model, TFAutoModelForCausalLM
-tokenizer_gpt2 = GPT2Tokenizer.from_pretrained('gpt2', cache_dir="./cache", local_files_only=True)
-#tokenizer_gpt2.padding_side = "left" 
-tokenizer_gpt2.pad_token = tokenizer_gpt2.eos_token # to avoid an error "<|endoftext|>": 50256
-tokenizer_gpt2.sep_token = '<|sep|>'
-#tokenizer_gpt2.add_tokens(tokenizer_gpt2.sep_token)
-print(tokenizer_gpt2)
-gpt2 = GPT2LMHeadModel.from_pretrained('gpt2', cache_dir="./cache", local_files_only=True)
 
-gpt2.trainable = False
-gpt2.config.pad_token_id=50256
-gen_nlp  = pipeline("text-generation", model=gpt2, tokenizer=tokenizer_gpt2, device=0, return_full_text=False)
+if args.genm == 'gpt2':
+    from transformers import GPT2Tokenizer, GPT2LMHeadModel #TFGPT2LMHeadModel, TFGPT2Model, TFAutoModelForCausalLM
+    tokenizer_gpt2 = GPT2Tokenizer.from_pretrained('gpt2', cache_dir="./cache", local_files_only=True)
+    #tokenizer_gpt2.padding_side = "left" 
+    tokenizer_gpt2.pad_token = tokenizer_gpt2.eos_token # to avoid an error "<|endoftext|>": 50256
+    tokenizer_gpt2.sep_token = '<|sep|>'
+    #tokenizer_gpt2.add_tokens(tokenizer_gpt2.sep_token)
+    print(tokenizer_gpt2)
+    gpt2 = GPT2LMHeadModel.from_pretrained('gpt2', cache_dir="./cache", local_files_only=True)
+
+    gpt2.trainable = False
+    gpt2.config.pad_token_id=50256
+    gen_nlp  = pipeline("text-generation", model=gpt2, tokenizer=tokenizer_gpt2, device=0, return_full_text=False)
+
+elif args.genm == 't5':
+    from transformers import T5Tokenizer, AutoModelWithLMHead
+    tokenizer_t5 = T5Tokenizer.from_pretrained("t5-base", cache_dir="./cache", local_files_only=True)
+    print(tokenizer_t5)
+    t5 = AutoModelWithLMHead.from_pretrained("t5-base", cache_dir="./cache", local_files_only=True)    
+    gen_nlp  = pipeline("text2text-generation", model=t5, tokenizer=tokenizer_t5, device=0)
+
 
 
 # gpt expand
@@ -65,11 +77,11 @@ for ix, row in ds.df_train.reset_index().iterrows():
         accs_noexpand.append(0)
 
 
-    result_gpt = gen_nlp([remove_str(content)], max_length=64, \
+    result_gpt = gen_nlp([remove_str(content)], max_length=args.maxlen, \
                                         do_sample=True, top_p=0.9, top_k=0, temperature=1.2,\
                                         repetition_penalty=1.2, num_return_sequences= args.fbs_gen,\
                                         clean_up_tokenization_spaces=True)
-     
+    
     ori_gen_contents = [ii['generated_text'] for ii in result_gpt if ii['generated_text']] + [remove_str(content)]
     nli_result = nli_nlp(ori_gen_contents,  labels_candidates, multi_label=True, hypothesis_template="This text is about {}.")
     ls = {l:0 for l in labels_candidates}
@@ -87,7 +99,8 @@ for ix, row in ds.df_train.reset_index().iterrows():
     if ix % 2048 == 0 and ix > 0:
         print(ix, sum(accs_noexpand) / len(accs_noexpand), sum(accs_expand)/len(accs_expand))
 
-print("summary==>", args.fbs_gen, sum(accs_noexpand) / len(accs_noexpand), sum(accs_expand)/len(accs_expand) )
+print("summary==>", ' '.join(['{}:{}'.format(k, v) for k, v in vars(args).items()]) ,
+             sum(accs_noexpand) / len(accs_noexpand), sum(accs_expand)/len(accs_expand) )
 
 
 
