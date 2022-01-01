c0ede68251818205d08ee58976e53a10dfe018fd
yanan <yanan@v1040-wn-rt-a-44-86.campus-dynamic.uwaterloo.ca>
2022-01-01 15:23:49 -0500

    fix augf

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ do
 				python -u augf.py --dsn ${1} --samplecnt ${samplecnt} --max_aug_times 1 --aug generate \
 						      --genft no  --genm ${genm} --filter nlinsp --seed ${seed} --testbed 0 \
 						      --testvalid test --candidates ${candidates} --gpu ${2} \
-			> ./log_arxiv_nlinsp_noner/${dsn}.generate.${samplecnt}.${max_aug_times}.${candidates}.${genm}.no.${seed}.log 2>&1
+			> ./log_arxiv_nlinsp_noner/${1}.generate.${samplecnt}.${max_aug_times}.${candidates}.${genm}.no.${seed}.log 2>&1
 			done
 	        ## end 
 			
