cb0a1fbed94809df5a7d96d45d0c59c91a7c5fab
yanan <yanan@v1040-wn-rt-a-21-88.campus-dynamic.uwaterloo.ca>
2022-02-04 18:00:58 -0500

    fix robert zsl seed words

---
+++ utils/seed_words.py
@@ -0,0 +1,159 @@
+
+
+from transformers.file_utils import cached_path
+# gram_diff = joblib.load("gram_diff___{}".format('ag'))
+# model_w2v = gensim.models.KeyedVectors.load_word2vec_format('./resource/GoogleNews-vectors-negative300.bin',binary=True)
+
+
+
+label_expands_self ={
+    'politics':['Politics','War', 'Election','Constitution','Democracy','Conflict','Military', 'Peace', 'Iran', \
+                'Syria', 'Israel', 'Crisis', 'EU','NATO', 'korea', 'Afghanistan', 'Taliban',
+                'Terrorism', 'Government', 'Ideology', 'fascism', 'Socialism', 'Totalitarian', 'Religion'],
+
+    'law':      ['Law', 'Legitimacy','Court','Crime','Murder','Jurisdiction'],
+
+    'science':  ['Science','Aerospace','Physics','Chemistry','Biology','Scientist','Astronomy','Universe','Big Bang'],
+
+    'technology':['Technology','Biotech', 'IT','Computers','Internet','Algorithm','Space','Bitcoin',\
+                    'artificial Intelligence','Robot'],
+
+    'health': ['Health','Healthcare','Medicine','Clinics','Vaccine','Wellness','Nutrition','Dental','HIV','Disease'],
+
+    'business': ['Business','Finance','Oil price','Supply','Inflation','Dollars','Bank','Wall Street','Bitcoin',
+                        'Federal Reserve','Accrual','Accountancy','Sluggishness','Consumerism','Trade','Quarterly earnings',\
+                         'Deposit','Revenue','Stocks','Recapitalization','Marketing','Futures', 
+                         'startups', 'competition', 'billionaire', 'worker', 'retailer', 'entrepreneurs', 'merger', \
+                    'acquisition', 'manufacturers', 'businesses', 'economist', 'sponsor', 'broker', 'growth',\
+                     'corporation', 'sells', 'giants', 'profit', 'trader', 'brands', 'clients', 'manufacturing', \
+                     'pharma', 'bank', 'investor', 'execs', 'market', 'client', 'shareholder', 'employers',\
+                      'earnings', 'businesswoman', 'marketing', 'entrepreneur', 'directors', 'dollar',\
+                       'professional', 'manager', 'finance', 'estate', 'warehouse', 'companies', 'distributor', \
+                       'chain', 'sales', 'supplier', 'commerce', 'robbery', 'china', 'factory', 'retail', \
+                       'workplace', 'brewers', 'bosses', 'production', 'traders', 'operations', 'executive',\
+                        'productivity', 'contractors', 'corp', 'stocks', 'managers', 'employment', 'division',\
+                         'recruitment', 'employee', 'mining', 'profits', 'employees', 'manufacturer', 'partnership', \
+                         'contractor', 'boss', 'shareholders', 'team', 'enterprise', 'investors', 'workers', 'marketers',\
+                          'economy', 'ceo', 'shares', 'services', 'franchise', 'businessman',\
+                    'exporters', 'partners', 'conference', 'career', 'commercial', 'markets', 'tycoon', 'meeting', \
+                    'partner', 'retailers', 'launches', 'executives', 'insurer', 'startup', 'employer', 'buys', 'dealer', \
+                    'workplaces', 'industries', 'technology', 'trading', 'customers', 'contract', 'street', 'analysts', 'stores',\
+                      'dividend', 'bucks', 'stock', 'supermarket', 'capital', 'customer', 'banks', 'equities', \
+                     'jobs', 'economics', 'revenue', 'investment', 'hires', 'success', 'rate', 'angel funds', 'Venture Capital', 
+                     'VC'],
+
+
+    'sports': ['Sports','Athletics','Championships','Football','Olympic','Tournament','Chelsea','League','Golf',
+                            'NFL','Super bowl','World Cup', 'formula',\
+                            'marathon', 'curling', 'athlete', 'riding', 'swimmers', 'tennis', 'players', 'manchester',
+                    'cyclists', 'scores', 'olympian', 'nike', 'footballers', 'liverpool', 'soccer',
+                    'climber', 'receiver', 'hoops', 'pitching', 'volleyball', 
+                    'lineman', 'tournament', 'champions', 'rodeo', 'pitcher', 'stunt', 'stars',
+                    'gym', 'weight', 'softball', 'quarterback', 'colts', 
+                     'cyclist', 'bike', 'surfer', 'marlins', 'championship', 'hockey', 
+                     'sportsman', 'matches', 'darts', 'broncos', 'bets',
+                      'workout', 'titans', 'skiers', 'playoff', 'footballer', 'teams', 'redskins', 
+                     'barcelona', 'swimming', 'skate', 'speedway', 'bikes', 'golfers', 
+                     'champion', 'cowboys', 'raceway', 'coaching', 'pitchers', 'swimsuit', 
+                'atlanta', 'surfing', 'cycling', 'baseball', 'season', 'racing', 'gymnastics',
+                 'lineup', 'bowling', 'finals', 'wrestling', 'trainer', 'playoffs', 'boxing', 
+                 'teammates', 'caps', 'fans', 'wrestler', 'chess', 'fitness', 'lacrosse',
+                  'dancers', 'trophy', 'athletes', 'wrestlers', 'batting', 'club',  'training',
+                  'yankees', 'basketball', 'jones', 'olympics', 'cricketer',
+                   'team',  'league', 'cricketers', 'golfer', 'exercise', 'derby', 'workouts',
+                    'match', 'jersey', 'riders', 'coaches', 'swim', 'bowl', 'skating', 'coach', 
+                    'linebacker', 'stadium', 'diving', 'boxer', 'captain', 'skateboard', 'golf',
+                     'athletics', 'runners', 'rugby', 'wimbledon', 'quarterbacks', 'cricket', 'columbia'],
+
+    'science and technology': ['equipment', 'smart', 'rocket', 'weld', 'tools', 'submarine', 'materials', 'lego', 'battery', \
+                    'uranium', 'android', 'transplant', 'experiment', 'researchers', 'nasa', 'nobel prize',\
+                     'internet', 'chip', 'newton', 'innovation', 'engineering', 'telecom', 'silicon', \
+                     'moon', 'software', 'speed', 'giants', 'puppet', 'development', 'phones', 'arsenal', \
+                      'cameras', 'streaming', 'rockets', 'developers', 'robots', 'manufacturing', 'zoo', 'pharma', \
+                      'revolution', 'mobile', 'balloon', 'brain', 'hacker', 'gps', 'toys', 'weapons', 'titans',\
+                       'documentary', 'synthetic', 'cybercrime', 'electric', 'coal', 'marvel', 'cellphone', 'energy',\
+                        'factories', 'electronics', 'builds', 'laboratories', 'google', 'scientists', 'hackers',\
+                         'laptop', 'space', 'trump', 'study', 'robotics', 'pilot', 'diesel', 'researcher', 'pipeline',\
+                          'rays', 'nuke', 'chemistry', 'semiconductor', 'machines', 'camera', 'cloud', 'penguins',\
+                           'chargers', 'mining', 'engineers', 'chemical', 'gaming', 'technical', 'olympics', 'nukes',\
+                            'hack', 'drilling', 'aircraft', 'drones', 'model', 'galaxy', 'science', 'machine', \
+                            'technologies', 'tesla', 'weapon', 'hacks', 'tool', 'launches', 'electricity',\
+                     'screening', 'startup', 'laser', 'engineer', 'patent', 'future', 'testing', 'scientist',\
+                      'discovery', 'wireless', 'technology', 'breakthrough', 'video', 'glasses', 'launch', 'satellite',\
+                       'biotech', 'probe', 'magic', 'robot', 'training', 'apple', 'automation', 'artificial Intelligence', 'Musk',
+                    'mars', 'solar system', 'arxiv', 'quantum', 'computing', 'nuclear']
+}
+
+
+
+     
+
+def get_s3_words():
+    BAG_OF_WORDS_ARCHIVE_MAP = {
+        'legal': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/legal.txt",
+        'military': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/military.txt",
+        'monsters': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/monsters.txt",
+        'politics': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/politics.txt",
+        'positive_words': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/positive_words.txt",
+        'religion': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/religion.txt",
+        'science': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/science.txt",
+        'space': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/space.txt",
+        'technology': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/technology.txt",
+        }
+    label_expands = {}
+    for l in BAG_OF_WORDS_ARCHIVE_MAP.keys():
+        filepath = cached_path(BAG_OF_WORDS_ARCHIVE_MAP[l])
+        with open(filepath, "r") as f:
+            words = f.read().strip().split("\n")
+        #print(words)
+        label_expands[l] = words
+    return label_expands
+
+label_expands_s3 =  get_s3_words()
+
+
+
+'''
+def get_seed_words(topk):
+    vocab_w2v = set(list(model_w2v.index_to_key))
+    label_expands_auto = {}
+    for l, gram_scores in gram_diff.items():
+        gram_scores_sum = {g:round(np.array(scores).sum(),4) for g, scores in gram_scores.items() }
+        gram_scores_sum_sort = sorted(gram_scores_sum.items(), key=operator.itemgetter(1), reverse=True) 
+        gram_scores_mean = {g:round(np.array(scores).mean(),4) for g, scores in gram_scores.items() }
+        gram_scores_mean_sort = sorted(gram_scores_mean.items(), key=operator.itemgetter(1), reverse=True) 
+        gram_scores_sort = gram_scores_sum_sort + gram_scores_mean_sort
+        label_expands_auto[l] = set()
+        for j in gram_scores_sort:
+            if j[0] not in vocab_w2v or j[0] in ['news']:
+                #print(j[0])
+                continue
+            if ' and ' in l:
+                w0 = l.split('and')[0].strip().lower()
+                w1 = l.split('and')[1].strip().lower()
+                simi = max(model_w2v.similarity(w0, j[0]), model_w2v.similarity(w1, j[0]) )
+            else:
+                simi = model_w2v.similarity(l.lower(), j[0])
+            if simi >= 0.1:
+                label_expands_auto[l].add(j[0])
+            if len(label_expands_auto[l]) == topk:
+                break 
+        if ' and ' in l:
+            label_expands_auto[l].add(l.split('and')[0].strip())
+            label_expands_auto[l].add(l.split('and')[1].strip())
+        else:
+            label_expands_auto[l].add(l)
+        # for ll in BAG_OF_WORDS_ARCHIVE_MAP:
+        #     if (ll in l.lower()) or ('world' in l.lower() and ll == 'politics') or \
+        #         (('science' in l.lower() or 'technology' in l.lower()) and ll == 'space'):
+        #         words_s3 = get_s3_words(ll)
+        #         label_expands_auto[l].update(words_s3)
+        # for ll, expands in BASE_NLI.items():
+        #     if ll in l.lower():
+        #         label_expands_auto[l].update( [w.lower() for w in expands ] )
+        print(l, label_expands_auto[l], '\n')
+    return label_expands_auto
+'''
+
+# label_expands_auto = get_seed_words(128)
+# print(label_expands_auto)
--- zsl_gpt_nat.py
+++ zsl_gpt_nat.py
@@ -7,7 +7,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 import tensorflow as tf
 from sklearn.metrics.pairwise import cosine_distances,cosine_similarity 
 import joblib,gensim,transformers
-assert gensim.__version__ == '4.1.2'
+#assert gensim.__version__ == '4.1.2'
 os.environ['CUDA_VISIBLE_DEVICES'] = ''
 from transformers import AutoModelForCausalLM, AutoTokenizer, top_k_top_p_filtering
 import torch,operator
@@ -25,106 +25,50 @@ from transformers import pipeline
 
 
 
-from transformers import GPT2LMHeadModel, GPT2TokenizerFast
 
-import torch,transformers
-device = torch.device("cuda:{}".format(0) if torch.cuda.is_available() else "cpu")
-
-model = GPT2LMHeadModel.from_pretrained("./finetunes/gpt_nat_zsl/epoch_1_ppl_11.918800044184868").to(device)
-
-#model = GPT2LMHeadModel.from_pretrained("./finetunes/gpt2_natcat").to(device)
-
-tokenizer = GPT2TokenizerFast.from_pretrained('gpt2', cache_dir="./cache", local_files_only=True)
-
-# model.trainable = False
-# model.config.pad_token_id=50256
-# gen_nlp  = transformers.pipeline("text-generation", model=model, tokenizer=tokenizer, device=0, return_full_text=False)
-
-
-
-from transformers.file_utils import cached_path
-gram_diff = joblib.load("gram_diff___{}".format('ag'))
-model_w2v = gensim.models.KeyedVectors.load_word2vec_format('./resource/GoogleNews-vectors-negative300.bin',binary=True)
-
-BAG_OF_WORDS_ARCHIVE_MAP = {
-    'legal': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/legal.txt",
-    'military': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/military.txt",
-    'monsters': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/monsters.txt",
-    'politics': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/politics.txt",
-    'positive_words': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/positive_words.txt",
-    'religion': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/religion.txt",
-    'science': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/science.txt",
-    'space': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/space.txt",
-    'technology': "https://s3.amazonaws.com/models.huggingface.co/bert/pplm/bow/technology.txt",
-    }
-
-BASE_NLI ={
-    'politics':['Politics','War', 'Election','Constitution','Democracy','Conflict','Military',\
-                'Terrorism', 'Government', 'Ideology', 'fascism', 'Socialism', 'Totalitarian', 'Religion'],
-    'law':      ['Law', 'Legitimacy','Court','Crime','Murder','Jurisdiction'],
-    'science':  ['Science','Aerospace','Physics','Chemistry','Biology','Scientist','Astronomy','Universe','Big Bang'],
-    'technology':['Technology','Biotech', 'IT','Computers','Internet','Algorithm','Space','Bitcoin','artificial Intelligence','Robot'],
-    'health': ['Health','Healthcare','Medicine','Clinics','Vaccine','Wellness','Nutrition','Dental','HIV','Disease'],
-    'business': ['Business','Finance','Oil price','Supply','Inflation','Dollars','Bank','Wall Street','Bitcoin',
-                        'Federal Reserve','Accrual','Accountancy','Sluggishness','Consumerism','Trade','Quarterly earnings',\
-                         'Deposit','Revenue','Stocks','Recapitalization','Marketing','Futures'],
-    'sports': ['Sports','Athletics','Championships','Football','Olympic','Tournament','Chelsea','League','Golf',
-                            'NFL','Super bowl','World Cup']
-}
-
-def get_s3_words(ll):
-    filepath = cached_path(BAG_OF_WORDS_ARCHIVE_MAP[ll])
-    with open(filepath, "r") as f:
-        words = f.read().strip().split("\n")
-    return words
-
-def get_seed_words(topk):
-    vocab_w2v = set(list(model_w2v.index_to_key))
-    label_expands_auto = {}
-    for l, gram_scores in gram_diff.items():
-        gram_scores_sum = {g:round(np.array(scores).sum(),4) for g, scores in gram_scores.items() }
-        gram_scores_sum_sort = sorted(gram_scores_sum.items(), key=operator.itemgetter(1), reverse=True) 
-        gram_scores_mean = {g:round(np.array(scores).mean(),4) for g, scores in gram_scores.items() }
-        gram_scores_mean_sort = sorted(gram_scores_mean.items(), key=operator.itemgetter(1), reverse=True) 
-        gram_scores_sort = gram_scores_sum_sort + gram_scores_mean_sort
-        label_expands_auto[l] = set()
-        for j in gram_scores_sort:
-            if j[0] not in vocab_w2v or j[0] in ['news']:
-                #print(j[0])
-                continue
-            if ' and ' in l:
-                w0 = l.split('and')[0].strip().lower()
-                w1 = l.split('and')[1].strip().lower()
-                simi = max(model_w2v.similarity(w0, j[0]), model_w2v.similarity(w1, j[0]) )
-            else:
-                simi = model_w2v.similarity(l.lower(), j[0])
-            if simi >= 0.1:
-                label_expands_auto[l].add(j[0])
-            if len(label_expands_auto[l]) == topk:
-                break 
-        if ' and ' in l:
-            label_expands_auto[l].add(l.split('and')[0].strip())
-            label_expands_auto[l].add(l.split('and')[1].strip())
-        else:
-            label_expands_auto[l].add(l)
-        for ll in BAG_OF_WORDS_ARCHIVE_MAP:
-            if (ll in l.lower()) or ('world' in l.lower() and ll == 'politics') or \
-                (('science' in l.lower() or 'technology' in l.lower()) and ll == 'space'):
-                words_s3 = get_s3_words(ll)
-                label_expands_auto[l].update(words_s3)
-        for ll, expands in BASE_NLI.items():
-            if ll in l.lower():
-                label_expands_auto[l].update( [w.lower() for w in expands ] )
-        print(l, label_expands_auto[l], '\n')
-    return label_expands_auto
-
-
-label_expands_auto = get_seed_words(64)
-print(label_expands_auto)
+from utils.seed_words import *
 
 stopwords = joblib.load("./utils/stopwords")
 stopwords = set(stopwords)  
 
+id_token = {ix:token for token, ix in tokenizer.vocab.items()}
+
+
+
+for l, w in label_expands_auto.items():
+
+
+
+from utils.load_data import * 
+
+ds = load_data(dataset='ag', samplecnt= -1)
+labels_candidates = ds.df_train['label_name'].unique().tolist()
+
+
+
+
+
+for ix, row in ds.df_test.sample(frac=1).iterrows():
+    content = row['content'] 
+
+
+template1 = "{}. This News is about {}".format(content, nlp_fill.tokenizer.mask_token)
+template2 = "{} News: {}.".format(nlp_fill.tokenizer.mask_token, content)
+template3 = "[Category: {} ] {}.".format(nlp_fill.tokenizer.mask_token, content)
+
+ls = {l:0 for l in labels_candidates}
+
+filled_results = nlp_fill([template1, template2, template3])
+
+
+
+
+
+
+from transformers import AutoTokenizer, AutoModelWithLMHead
+tokenizer = AutoTokenizer.from_pretrained("roberta-large",cache_dir="./cache",local_files_only=True) 
+model = AutoModelWithLMHead.from_pretrained("roberta-large",cache_dir="./cache",local_files_only=True)
+nlp_fill = pipeline("fill-mask", model=model, tokenizer=tokenizer, device=-1, top_k = len(tokenizer.vocab) ) 
 id_token = {ix:token for token, ix in tokenizer.vocab.items()}
 
 
@@ -138,10 +82,12 @@ id_token = {ix:token for token, ix in tokenizer.vocab.items()}
 
 
 
-from utils.load_data import * 
 
-ds = load_data(dataset='yahoo', samplecnt= 8)
-labels_candidates = ds.df_train['label_name'].unique().tolist()
+
+
+
+
+
 
 
 def gpt4zsl_pred(sent):
